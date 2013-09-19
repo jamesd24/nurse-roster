fb54bddf0548e37835f5efb19155d432aad017d0
James Dahms <jamesdahms@gmail.com>
2013-09-19 13:28:38 +1000

    added gitignore and added main class for app with simple window

---
+++ src/InitialWindowTest.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: James
+ * Date: 9/19/13
+ * Time: 1:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class InitialWindowTest extends JFrame {
+
+    public InitialWindowTest() {
+
+        setTitle("Nurse Rosters");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
+
---
+++ src/NurseRosterApp.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: James
+ * Date: 9/19/13
+ * Time: 1:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NurseRosterApp {
+    public static void main(String[] args) {
+
+        InitialWindowTest test = new InitialWindowTest();
+        test.setVisible(true);
+
+        //System.out.println("Hello World!"); // Display the string.
+    }
+}
