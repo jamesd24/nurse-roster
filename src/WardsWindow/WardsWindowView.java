dd3baba5dcb09d7ab7e8547888d1ffa5217496be
James Dahms <jamesdahms@gmail.com>
2013-09-29 16:10:10 +1000

    Changed WardsWindow to represent design

--- src/WardsWindow/WardsWindowView.java
+++ src/WardsWindow/WardsWindowView.java
@@ -14,6 +14,7 @@ package WardsWindow;
 // information entered by the user to whomever needs
 // it.
 
+import java.awt.*;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
@@ -21,27 +22,57 @@ import javax.swing.*;
 public class WardsWindowView extends JFrame{
 
     private JTextField firstNumber  = new JTextField(10);
-    private JLabel additionLabel = new JLabel("+");
     private JTextField secondNumber = new JTextField(10);
-    private JButton calculateButton = new JButton("Calculate");
+    private JButton newWardButton = new JButton("New Ward");
+    private JButton propertiesButton = new JButton("Properties");
+    private JButton rosterButton = new JButton("Roster");
+    private JButton deleteWardeButton = new JButton("Delete Ward");
+    private JButton calculateButton = new JButton("Calc");
     private JTextField calcSolution = new JTextField(10);
+    private JList wardsList = new JList();
+    private JScrollPane wardsPane = new JScrollPane();
 
     WardsWindowView(){
 
         // Sets up the view and adds the components
+        // Without using GUI Form designer (BOSS!)
 
-        JPanel calcPanel = new JPanel();
+        JPanel listPanel = new JPanel();
+        JPanel buttonsPanel = new JPanel();
+        JLabel titleLabel = new JLabel("Wards");
+        titleLabel.setFont(new java.awt.Font("Tahoma", 0, 24));
 
+        this.setTitle("Nurse Rosters");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(600, 200);
+        this.setSize(640, 430);
+        this.setResizable(false);
+        this.add("North", titleLabel);
 
-        calcPanel.add(firstNumber);
-        calcPanel.add(additionLabel);
-        calcPanel.add(secondNumber);
-        calcPanel.add(calculateButton);
-        calcPanel.add(calcSolution);
+        wardsList.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Ward 1", "Ward 2", "Ward 3", "Ward 4", "Ward 5","Ward 6","Ward 7","Ward 8","Ward 9",  };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        wardsPane.setViewportView(wardsList);
+        wardsPane.setPreferredSize(new Dimension(500, 370));
 
-        this.add(calcPanel);
+        listPanel.add(wardsPane);
+
+        newWardButton.setPreferredSize(new Dimension(120,30));
+        propertiesButton.setPreferredSize(new Dimension(120,30));
+        rosterButton.setPreferredSize(new Dimension(120,30));
+        deleteWardeButton.setPreferredSize(new Dimension(120,30));
+
+        buttonsPanel.add(newWardButton);
+        buttonsPanel.add(propertiesButton);
+        buttonsPanel.add(rosterButton);
+        buttonsPanel.add(deleteWardeButton);
+
+        listPanel.setPreferredSize(new Dimension(500,370));
+        buttonsPanel.setPreferredSize(new Dimension(130,370));
+
+        this.add("West",listPanel);
+        this.add("East", buttonsPanel);
 
         // End of setting up the components --------
 
